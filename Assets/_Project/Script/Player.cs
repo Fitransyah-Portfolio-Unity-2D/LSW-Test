ce91da0e90058cccd733f9c233d125965b99557e
Fitransyah <fitransyah.rusman@gmail.com>
2022-08-26 15:10:22 +0700

    walk animation x direction

--- Assets/_Project/Script/Player.cs
+++ Assets/_Project/Script/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D playerRB;
     Animator playerAnimator;
+    SpriteRenderer playerSprite;
 
     [SerializeField]
     float walkSpeed;
@@ -16,6 +17,8 @@ public class Player : MonoBehaviour
         playerRB.gravityScale = 0;
 
         playerAnimator = GetComponent<Animator>();
+
+        playerSprite = GetComponent<SpriteRenderer>();
         
     }
 
@@ -37,19 +40,52 @@ public class Player : MonoBehaviour
         float x = moveInput.x;
         float y = moveInput.y;
 
+        Vector2 lastFacingDirection = new Vector2();
+
         if (y > 0)
         {
+
             playerAnimator.SetBool("isWalkingBack", true);
+
+            lastFacingDirection = new Vector2((playerRB.velocity.x), (playerRB.velocity.y));
         }
         else if (y < 0)
         {
+
             playerAnimator.SetBool("isWalkingFront", true);
+
+            lastFacingDirection = new Vector2((playerRB.velocity.x), (playerRB.velocity.y));
+        }
+        else if (x > 0)
+        {
+
+            playerAnimator.SetBool("isWalkingSide", true);
+
+            lastFacingDirection = new Vector2((playerRB.velocity.x), (playerRB.velocity.y));
+        }
+        else if (x < 0)
+        {
+            playerSprite.flipX = true;
+            playerAnimator.SetBool("isWalkingSide", true);
+
+            lastFacingDirection = new Vector2((playerRB.velocity.x), (playerRB.velocity.y));
         }
         else
         {
+            playerSprite.flipX = false;
             playerAnimator.SetBool("isWalkingFront", false);
             playerAnimator.SetBool("isWalkingBack", false);
+            playerAnimator.SetBool("isWalkingSide", false);
+
+            switch (lastFacingDirection)
+            {
+                // TO DO :
+                // Set default sprite to last facing direction
+                // stop idle animation if character facing right, left or up
+            }
         }
+
+        Debug.Log("Last facing direction : " + lastFacingDirection);
     }
 
 }
