fc72e2fd06bc3b0544698a04fdd061eb1e7e8e42
Fitransyah <fitransyah.rusman@gmail.com>
2022-08-26 14:21:48 +0700

    2D movement for player

--- Assets/_Project/Script/Player.cs
+++ Assets/_Project/Script/Player.cs
@@ -6,16 +6,20 @@ public class Player : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D playerRB;
+
+    [SerializeField]
+    float walkSpeed;
     void Start()
     {
         playerRB =  GetComponent<Rigidbody2D>();
-        playerRB.isKinematic = true;
+        playerRB.gravityScale = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Walk();
     }
 
     void OnMove(InputValue value)
@@ -24,4 +28,14 @@ public class Player : MonoBehaviour
 
         Debug.Log(moveInput);
     }
+    void Walk()
+    {
+        playerRB.velocity = moveInput * walkSpeed;
+        float x = moveInput.x;
+        float y = moveInput.y;
+
+        Debug.Log("X : " + x);
+        Debug.Log("Y : " + y);
+    }
+
 }
