a36cea9d9322720a248d0e3346e5a12bd8888c6f
Fitransyah <fitransyah.rusman@gmail.com>
2022-08-29 20:03:15 +0700

    finalize interaction system

--- Assets/_Project/Script/Interaction/InteractObject.cs
+++ Assets/_Project/Script/Interaction/InteractObject.cs
@@ -1,30 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InteractObject : MonoBehaviour
 {
     [SerializeField] ExamineInteraction examineInteraction;
+    [SerializeField] TMP_Text infoPanelText;
+
+    string startingInfoPanelText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log(collision.gameObject.name);
+            infoPanelText = GameObject.FindGameObjectWithTag("InfoPanel").GetComponent<TMP_Text>();
+            startingInfoPanelText = infoPanelText.text;
+            StartCoroutine(DisplayExamineDescription(examineInteraction.GetDescriptionList()));
         }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            Debug.Log(collision.gameObject.name);
-        }
-    }
+    } 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log(collision.gameObject.name);
+            StopCoroutine(DisplayExamineDescription(examineInteraction.GetDescriptionList()));
+            infoPanelText.text = startingInfoPanelText;
         }
     }
+    public IEnumerator DisplayExamineDescription(List<string> examineDescriptions)
+    {
+        for (int i = 0; i < examineDescriptions.Count; i++)
+        {
+            infoPanelText.text = examineDescriptions[i];
+
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        }
+    }
+
 }
