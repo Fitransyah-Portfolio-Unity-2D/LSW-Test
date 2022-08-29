93b543f656e4ad3b0308d2a0fd50d34726dd5e8c
Fitransyah <fitransyah.rusman@gmail.com>
2022-08-30 00:05:00 +0700

    implementing GameDev.TV drag and drop framework part 1

--- Assets/_Project/Script/Inventory/InventoryItem.cs
+++ Assets/_Project/Script/Inventory/InventoryItem.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InventoryItem : MonoBehaviour
+namespace LSWTest.Inventory
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InventoryItem : MonoBehaviour
     {
-        
-    }
+        /// <summary>
+        /// First will check if parameter is null or not.
+        /// If null  will disable the Image component, if not null will set up sprite with parameters and enable the game object.
+        /// </summary>
+        /// <param name="item">Sprite to set in the Image component.</param>
+        public void SetItem(Sprite item)
+        {
+            var iconImage = GetComponent<Image>();
+            if (item == null)
+            {
+                iconImage.enabled = false;
+            }
+            else
+            {
+                iconImage.enabled = true;
+                iconImage.sprite = item;
+                Destroy(iconImage);
+            }
+        }
+        /// <summary>
+        /// If this script is not enable will return null, if enabled will return attached sprite
+        /// </summary>        
+        /// <returns> <strong>The Sprite currently in Image component</strong> </returns>
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public Sprite GetItem()
+        {
+            var iconImage = GetComponent<Image>();
+            if (!iconImage.enabled)
+            {
+                return null;
+            }
+            return iconImage.sprite;
+        }
     }
 }
+
--- Assets/_Project/Script/Inventory/InventorySlot.cs
+++ Assets/_Project/Script/Inventory/InventorySlot.cs
@@ -1,7 +1,42 @@
 using UnityEngine;
+using GameDevTV.Core.UI.Dragging;
 
-
-public class InventorySlot : MonoBehaviour
+namespace LSWTest.Inventory
 {
-    
+    public class InventorySlot : MonoBehaviour, IDragContainer<Sprite>
+    {
+        [SerializeField]
+        InventoryItem itemIcon;
+        public void AddItems(Sprite item, int number)
+        {
+            itemIcon.SetItem(item);
+        }
+
+        public Sprite GetItem()
+        {
+            return itemIcon.GetItem();
+        }
+
+        public int GetNumber()
+        {
+            // temporarily will return 1 untill we are working with
+            // stackable items
+            return 1;
+        }
+
+        public int MaxAcceptable(Sprite item)
+        {
+            if (GetItem() == null)
+            {
+                return int.MaxValue;
+            }
+            return 0;
+        }
+
+        public void RemoveItems(int number)
+        {
+            itemIcon.SetItem(null);
+        }
+    }
 }
+
