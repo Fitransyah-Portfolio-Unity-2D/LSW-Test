9b87d5ca5e326a7f0633f94ca36cd2b3ef2b20fc
Fitransyah <fitransyah.rusman@gmail.com>
2022-08-31 21:48:31 +0700

    setup drop item system

---
+++ Assets/_Project/Script/Inventory/InventoryDropTarget.cs
@@ -0,0 +1,26 @@
+using GameDevTV.Core.UI.Dragging;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSWTest.Inventory
+{
+    /// <summary>
+    /// Handles dropping item use previous drag and drop system
+    /// Placed in root canvas where UI inventory reside
+    /// </summary>
+    public class InventoryDropTarget : MonoBehaviour, IDragDestination<Item>
+    {
+        public void AddItems(Item item, int number)
+        {
+            var player = GameObject.FindWithTag("Player");
+            
+        }
+
+        public int MaxAcceptable(Item item)
+        {
+            return int.MaxValue;
+        }
+    }
+}
+
