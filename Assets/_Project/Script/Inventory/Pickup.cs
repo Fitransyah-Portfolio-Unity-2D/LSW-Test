14001b9bfade5f6c371767e93b832f21b88ea31c
Fitransyah <fitransyah.rusman@gmail.com>
2022-08-31 15:02:37 +0700

    starting inventory pickup system

---
+++ Assets/_Project/Script/Inventory/Pickup.cs
@@ -0,0 +1,13 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSWTest.Inventory
+{
+    public class Pickup : MonoBehaviour
+    {
+        Item item;
+        Inventory inventory;
+    }
+}
+
---
+++ Assets/_Project/Script/Inventory/PickupSpawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSWTest.Inventory
+{
+    public class PickupSpawner : MonoBehaviour
+    {
+        [SerializeField] Item item = null;
+    }
+}
+
